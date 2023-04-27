d4cd24faec4b2d03c927bc3b83363c6480f3e4f8
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-04-27 20:21:35 +0800

    仅对会导致崩溃的信号处理

--- bao-library/src/main/java/com/storyteller_f/bao_library/Bao.kt
+++ bao-library/src/main/java/com/storyteller_f/bao_library/Bao.kt
@@ -9,6 +9,46 @@ import android.os.Looper
 import android.util.Log
 import java.io.File
 
+object LinuxSig {
+    const val hup = 1
+    const val int = 2
+    const val quit = 3
+    const val ill = 4
+    const val trap = 5
+    const val abrt = 6
+    const val bus = 7
+    const val fpe = 8
+    const val kill = 9
+    const val usr1 = 10
+    const val segv = 11
+    const val usr2 = 12
+    const val pipe = 13
+    const val alrm = 14
+    const val term = 15
+    const val stkflt = 16
+    const val chld = 17
+    const val cont = 18
+    const val stop = 19
+    const val tstp = 20
+    const val ttin = 21
+    const val ttou = 22
+    const val urg = 23
+    const val xcpu = 24
+    const val xfsz = 25
+    const val vtalrm = 26
+    const val prof = 27
+    const val winch = 28
+    const val io = 29
+    const val pwr = 30
+    const val sys = 31
+    val appFatalSig = listOf(abrt, bus, fpe, ill, int, pipe, segv, term)
+    val ignoreSig = listOf(chld, urg, winch)
+    val terminateSig = listOf(hup, int, kill, usr1, usr1, usr2, pipe, alrm, term, stkflt, vtalrm, prof, io, pwr)
+    val coreSig = listOf(quit, ill, trap, abrt, bus, fpe, segv, xcpu, xfsz, sys)
+    val stopSig = listOf(stop, tstp, ttin, ttou)
+    val continueSig = listOf(cont)
+}
+
 fun Context.defaultBaoHandler(it: Throwable?): Boolean {
     Thread {
         Thread.sleep(500)
@@ -32,7 +72,10 @@ fun Context.stringBaoHandler(it: Throwable?): Boolean {
 /**
  * Throwable 如果为null，说明异常信息存储在cache.txt 中。使用`Bao.readException(context)` 读取。
  */
-class Bao(val app: Context, val block: Context.(Throwable?) -> Boolean = Context::stringBaoHandler) {
+class Bao(
+    val app: Context,
+    val block: Context.(Throwable?) -> Boolean = Context::stringBaoHandler
+) {
     private val file by lazy {
         nativeExceptionFile(app)
     }
@@ -89,7 +132,7 @@ class Bao(val app: Context, val block: Context.(Throwable?) -> Boolean = Context
         }
         observer.start()
         transferNativeExceptionFilePath(file.absolutePath)
-        repeat(32) {
+        LinuxSig.appFatalSig.forEach {
             registerActionHandler(it)
         }
     }
